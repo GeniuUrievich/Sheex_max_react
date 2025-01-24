99f47aabbeb8e32da1d91f4a6ec5f4e571fc0219
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-01-24 17:00:56 +0300

    Create

---
+++ src/List_card.js
@@ -0,0 +1,78 @@
+import image from "./Image/image.jpg"
+
+const List_card = [
+    {
+        key: 1,
+        img: image,
+        name: "Кроссовки",
+        price: "14000 руб",
+        sex: "мужской",
+        size: "35"
+    },
+    {
+        key: 2,
+        img: image,
+        name: "Кроссовки",
+        price: "24000 руб",
+        sex: "женский",
+        size: "36"
+    },
+    {
+        key: 3,
+        img: image,
+        name: "Кроссовки",
+        price: "14000 руб",
+        sex: "мужской",
+        size: "37"
+    },
+    {
+        key: 4,
+        img: image,
+        name: "Кроссовки",
+        price: "14000 руб",
+        sex: "женский",
+        size: "38"
+    },
+    {
+        key: 5,
+        img: image,
+        name: "Кроссовки",
+        price: "14000 руб",
+        sex: "мужской",
+        size: "39"
+    },
+    {
+        key: 6,
+        img: image,
+        name: "Кроссовки",
+        price: "34000 руб",
+        sex: "женский",
+        size: "40"
+    },
+    {
+        key: 7,
+        img: image,
+        name: "Кроссовки",
+        price: "4000 руб",
+        sex: "мужской",
+        size: "41"
+    },
+    {
+        key: 8,
+        img: image,
+        name: "Кеды",
+        price: "45000 руб",
+        sex: "женский",
+        size: "42"
+    },
+    {
+        key: 9,
+        img: image,
+        name: "Кеды",
+        price: "56000 руб",
+        sex: "женский",
+        size: "43"
+    }
+]
+
+export default List_card
