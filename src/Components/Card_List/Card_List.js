6208960fd6567814a4ae84208cf26777c3a77338
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-01-24 22:47:45 +0300

    Update

--- src/Components/Card_List/Card_List.js
+++ src/Components/Card_List/Card_List.js
@@ -1,12 +1,23 @@
 import Card from "./../Card/Card";
 import "./../Card_List/style.css"
 import "./../../style.css"
+import Button_link from "../Button_link/Button_link";
 
 function Card_List({updata}) {
+    const margin =  {
+        marginLeft: "320px",
+        width: "200px",
+        marginTop: "40px",
+        marginBottom: "60px"
+    }
     return(
-        <div className="Card_list">
-            {updata.map((card) => {return <Card key={card.key} img={card.img} name={card.name} price = {card.price}></Card>})}
+        <div className="cards_list">
+            <div className="Card_list">
+                {updata.map((card) => {return <Card key={card.key} img={card.img} name={card.name} price = {card.price}></Card>})}
+            </div>
+            <Button_link style={margin}>Показать еще</Button_link>
         </div>
+        
     )
 }
 
