a872c355814c96117d8a0ad1514dbceaf951ab5e
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-01-24 22:47:32 +0300

    Update style

--- src/Components/Button_link/Button_link.js
+++ src/Components/Button_link/Button_link.js
@@ -1,9 +1,9 @@
 import "./../../style.css"
 
-function Button_link({children}){
+function Button_link({children, style}){
     return(
         <div>
-            <a className="button_link">{children}</a>
+            <a className="button_link" style={style}>{children}</a>
         </div>
     )
 }
