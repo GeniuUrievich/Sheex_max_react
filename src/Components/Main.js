d29a7cb33171911ef0ef57d9fb9029fd05147edb
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-01-24 17:03:15 +0300

    Update

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,11 +1,12 @@
-import Catalog_form from "./Catalog_form/Catalog_form";
 import Info_main from "./info_main/Info_main";
+import Card_List from "./Card_List/Card_List";
+import Catalog from "./Catalog/Catalog";
 
 function Main(){
     return(
         <main>
             <Info_main></Info_main>
-            <Catalog_form></Catalog_form>
+            <Catalog></Catalog>
         </main>
     )
 }
