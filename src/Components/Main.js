3e0e571b91fa2cafd897d6275efcfd3d1a12e995
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-01-25 22:15:06 +0300

    Update

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,6 +2,7 @@ import Info_main from "./info_main/Info_main";
 import Catalog from "./Catalog/Catalog";
 import About from "./About/About";
 import Quiz from "./Quiz/Quiz";
+import Team from "./Team/Team";
 
 function Main(){
     return(
@@ -10,6 +11,7 @@ function Main(){
             <Catalog></Catalog>
             <About></About>
             <Quiz></Quiz>
+            <Team></Team>
         </main>
     )
 }
