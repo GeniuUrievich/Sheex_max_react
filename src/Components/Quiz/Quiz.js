9172b3d2f2eaff2cbda1af8a53fe600dedd52313
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-01-25 22:15:17 +0300

    Refactoring

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,32 +1,36 @@
 import "./style.css"
 import "./../../style.css"
 import Quiz_Modal_1 from "../Quiz_Modal/Quiz_Modal_1"
+import Quiz_Modal_2 from "../Quiz_Modal/Quiz_Modal_2"
+import Quiz_Modal_3 from "../Quiz_Modal/Quiz_Modal_3"
 import { useState } from "react"
-import Quiz_Modal_list from "../Quiz_Modal/Quiz_Modal_list"
+import Quiz_Modal_All from "../Quiz_Modal/Quiz_Modal_All"
+import Quiz_Modal_Final from "../Quiz_Modal/Quiz_Modal_Final"
 
 function Quiz()  {
     const str = " из 3"
-    const [key, setKey] = useState(1)
+    const [step, setStep] = useState(1)
     const [mod, setMod]= useState(<Quiz_Modal_1/>)
     const click = () => {
-        if (key < 3){
-            setKey(key+1)   
-            setMod()         
+        if (step === 1){   
+            setMod(<Quiz_Modal_2></Quiz_Modal_2>)         
         }
+        else if (step===2){   
+            setMod(<Quiz_Modal_3></Quiz_Modal_3>)
+        }
+        setStep(step+1) 
     }
+
+    const resetQuiz = () => {
+        setStep(1)
+        setMod(<Quiz_Modal_1></Quiz_Modal_1>)
+    }
+
     return (
         <section className="sec_5">
             <div className="container">
                 <div className="quiz">
-                    <h1 className="quiz_text_h1">Мы подберем идеальную пару для вас</h1>
-                    <h3 className="quiz_text_h3">Ответьте на три вопроса и мы вышлем каталог с самыми подходящими для вас моделями </h3>
-                    <span className="quiz_line"></span>
-                    {mod}
-                    <span className="quiz_line"></span>
-                    <div className="footer_quiz">
-                        <h3 className="quiz_text_h4">{key+str}</h3>
-                        <button className="button_modal" onClick={click}>Следующий шаг</button>
-                    </div>
+                    {step < 4 ? <Quiz_Modal_All key={step} step={step} mod={mod} clickd={click} str={str}></Quiz_Modal_All> : <Quiz_Modal_Final resetQuiz={resetQuiz}></Quiz_Modal_Final>}
                 </div>
             </div>
         </section>
