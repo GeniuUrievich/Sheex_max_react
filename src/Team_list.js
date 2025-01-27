b084682b523dfaeeb5ec7a9fce50376a3c4c9b80
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-01-27 23:07:04 +0300

    Add

---
+++ src/Team_list.js
@@ -0,0 +1,41 @@
+import image from "./Image/Mask Group.jpg"
+import image2 from "./Image/Mask Group (1).jpg"
+import image3 from "./Image/Mask Group (2).jpg"
+import image4 from "./Image/Mask Group (3).jpg"
+import image5 from "./Image/Mask Group (4).jpg"
+import image6 from "./Image/Mask Group (5).jpg"
+
+const Team_List = [
+    {
+        img: image,
+        name: "Максим Золотарев",
+        role: "Администратор"
+    },
+    {
+        img: image2,
+        name: "Елена Добровольская",
+        role: "Консультант"
+    },
+    {
+        img: image3,
+        name: "Маргарита Коваль",
+        role: "Продавец"
+    },
+    {
+        img: image4,
+        name: "Алексей Лимонов",
+        role: "Продавец"
+    },
+    {
+        img: image5,
+        name: "Конрад Уелш",
+        role: "Консультант"
+    },
+    {
+        img: image6,
+        name: "Анастасия Вишня",
+        role: "Администратор"
+    }
+]
+
+export default Team_List
