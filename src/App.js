0e7378a525047edf2c6198515d7b1355d92b41e1
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-01-29 23:01:32 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import Card_open from './Components/Card_open/Card_open';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Main from './Components/Main';
+import { ModalProvider } from './ModalContext';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
-    </div>
+      <ModalProvider>
+        <div className="App"></div>
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+        <Card_open></Card_open>
+      </ModalProvider>
   );
 }
 
