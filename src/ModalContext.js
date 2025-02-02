40af2c2a8703bbe16a93bbdf49fe29c201d7c836
GeniuUrievich <158517318+GeniuUrievich@users.noreply.github.com>
2025-02-02 22:28:08 +0300

    Update

--- src/ModalContext.js
+++ src/ModalContext.js
@@ -3,28 +3,25 @@ import { createContext, useState, useContext } from "react";
 const ModalContext = createContext();
 
 export const ModalProvider = ({ children }) => {
-    const [isModalOpen, setModalOpen] = useState(false);
+    const [modalType, setModalType] = useState(null); 
+    const [modalData, setModalData] = useState(null);
 
-    const openModal = (data) => {
-        setModalData(data)
-        setModalOpen(true);
-    };
-
-    const [modalData, setModalData] = useState();
-
-    const closeModal = () => setModalOpen(false);
-
-    return (
-        <ModalContext.Provider value={{ isModalOpen, openModal, closeModal, modalData }}>
-            {children}
+    const [bascet, setBascet] = useState([])
+    const openModal = (type, data = null) => {
+        setModalType(type);
+        setModalData(data);
+      };
+    
+      const closeModal = () => {
+        setModalType(null);
+        setModalData(null);
+      };
+    
+      return (
+        <ModalContext.Provider value={{ modalType, modalData, openModal, closeModal, bascet, setBascet }}>
+          {children}
         </ModalContext.Provider>
-    );
+      );
 };
 
-export const useModal = () => {
-    const context = useContext(ModalContext);
-    if (!context) {
-        throw new Error("useModal должен использоваться внутри ModalProvider");
-    }
-    return context;
-};
+export const useModal = () => useContext(ModalContext)
